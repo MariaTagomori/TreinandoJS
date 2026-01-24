0228ebe406b6d20833b116095aaeed31db192d28
Miuke <wyverncampus@gmail.com>
2026-01-24 14:34:21 -0300

    treinando comandos básicos

---
+++ system.js/aula04/001.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+//mensagem de alerta
+window.alert("mensagem de alerta!");
+//mensagem de confirmação
+window.confirm("mensagem de confirmação?");
+//interação
+window.prompt("interação");
