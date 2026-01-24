4b72614c4d809bea620a534b5766bd4036baec7b
Miuke <wyverncampus@gmail.com>
2026-01-24 15:01:45 -0300

    aula 003

--- system.js/aula06/002.js
+++ system.js/aula06/002.js
@@ -1,5 +1,6 @@
 //pergunta o nome
-window.prompt("Qual é o seu nome?");
+//window.prompt("Qual é o seu nome?");
 
 //guardando a resposta do usuário na variável nome
 var nome = window.prompt("Qual é o seu nome?");
+window.alert("Prazer, " + nome);
---
+++ system.js/aula06/003.js
@@ -0,0 +1,2 @@
+var n1 = window.prompt("digite um número");
+var n2 = window.prompt("digite outro número");
