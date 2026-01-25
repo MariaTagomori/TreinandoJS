66a8839c1d05fa0ada77c268fe518e68257fba3a
Miuke <wyverncampus@gmail.com>
2026-01-25 15:10:48 -0300

    aula 07 - operadores

--- system.js/aula07/004.js
+++ system.js/aula07/004.js
@@ -19,6 +19,10 @@
 //**
 // * / %
 // + -
+//><>=
+//!
+//&&
+//||
 
 //atribuição simples
 var a = 5+3; //8
@@ -41,3 +45,64 @@ n = n % 5; //n%=5
 var x = 5
 x = x + 1; // x+=1 fica x++
 x = x - 1; //5
+
+//aula 08
+//relacionais
+//> maior que 
+//< menor que
+//>= maior ou igual
+//<= menor ou igual
+//== igualdade
+//!= diferente
+//== valor e tipo
+//!= valor e tipo
+
+//5 > 2 true
+// 7 < 4 false
+// 8 >= 8 true
+// 9 <= 7 false
+//5 == 5 = true
+// 4 != 4 false //DIFERENTE
+
+//exemplos
+preço >= 200.50
+idade < 18
+curso == "JavaScript"
+n1 != n2
+
+//identidade
+5 == 5 //true
+5 == '5' //true
+5 === 5 //true
+5 === '5' //false
+
+//lógicos
+//! negação 
+//&& conjunção (E)
+//|| disjunção
+
+//exemplos
+//!true = false
+//!false = true
+
+//true && true = true
+//true && false = false
+//false && true = false
+//false && false = false
+
+//true || true = true
+//true || false = true
+//false || true = true
+//false || false = false
+
+//idade >= 15 && idade <= 17 IDADE ENTRE 15 E 17?
+estado == "SP" || estado == "RJ" //ESTADO SP OU RJ?
+salário > 1500 && sexo != 'M' //  SALÁRIO MAIOR QUE 1500 E SEXO DIFERENTE DE M?
+
+//ternário
+//teste ? true : false
+//média >=? "Aprovado" : "Reprovado"
+
+exemplo
+var média = 5.6
+média > 7? "aprovado" : "reprovado"
