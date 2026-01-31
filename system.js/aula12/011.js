327c744c22caffefda96a86496741d8f72e90fa3
Miuke <wyverncampus@gmail.com>
2026-01-31 11:04:44 -0300

    Condições parte 2

---
+++ system.js/aula12/011.js
@@ -0,0 +1,20 @@
+//condições aninhandas
+//if (cond) {
+//bloco1
+//} else {
+//  if (cond2) {
+//bloco2
+//    } else {
+//bloco3
+ // }
+//}
+
+var idade = 17
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log("não vota")
+} else if (idade < 18) {
+        console.log("vota opcionalmente")
+    } else {
+        console.log("vota obrigatoriamente")
+    }
---
+++ system.js/aula12/013.js
@@ -0,0 +1,50 @@
+//condição múltipla
+
+//switch (expressão) {
+//case valor1:
+//break
+//case valor2:
+//break
+//case valor 3
+//break
+//default
+//break
+
+//valorespontuais
+
+var agora = new Date()
+var diaSem = agora.getDay()
+//0=domingo 
+// 1=segunda 
+// 2=terça
+// 3=quarta 
+// 4=quinta 
+// 5=sexta 
+// 6=sábado
+console.log(diaSem)
+switch (diaSem) {
+    case 0:
+        console.log("Domingo")
+        break
+    case 1:
+        console.log("Segunda-feira")
+        break
+    case 2:
+        console.log("Terça-feira")
+        break                       
+    case 3:
+        console.log("Quarta-feira")
+        break
+    case 4:
+        console.log("Quinta-feira")
+        break
+    case 5:
+        console.log("Sexta-feira")
+        break
+    case 6:
+        console.log("Sábado")
+        break
+    default:
+        console.log("[ERRO] Dia inválido!")
+        break
+}
