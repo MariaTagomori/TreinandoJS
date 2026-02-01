4c27022da9276937f64e6dfceab9415c3511f61a
Miuke <wyverncampus@gmail.com>
2026-02-01 11:11:33 -0300

    exercício de JS - hora do dia

--- system.js/exercícios/script.js
+++ system.js/exercícios/script.js
@@ -1,1 +1,20 @@
-alert("Olá, mundo!");
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours();
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    //BOM DIA
+    img.src = 'img/manhã.jpg'
+    document.body.style.background = '#fdeeac'
+} else if (hora >= 12 && hora < 18) {
+    //BOA TARDE
+    img.src = 'img/tarde.jpg'
+    document.body.style.background = '#e48c27'
+} else {
+    //BOA NOITE
+    img.src = 'img/noite.jpg'
+    document.body.style.background = '#115468'
+}
+}
